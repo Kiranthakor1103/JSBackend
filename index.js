e202aba9c02dd6cedab33abedb95ab44e067c762
THAKOR KIRANKUMAR RANJITSINH <thakorkiran1012003@gmail.com>
2024-12-24 12:45:15 +0530

    gitingorefile commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const express = require('express')
-require('dotenv').config()
 const app = express()
 const port = 3000
 
@@ -41,6 +41,6 @@ app.get('/github',(req,res)=>{
     res.json(githubData)
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT,() => {
   console.log(`Example app listening on port ${port}`)
 })
