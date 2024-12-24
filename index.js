3825b5981f86442dba14960e25f2f5a9de67493f
THAKOR KIRANKUMAR RANJITSINH <thakorkiran1012003@gmail.com>
2024-12-24 12:28:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+const githubData={
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('Twitter')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1> Welcome to login page</h1>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
